8fc82f59183fc8b878e2860f9ba6a07583d4a246
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-08-17 15:03:08 -0700

    organize business logic and UI logic in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,51 @@
-// business logic
+// BUSINESS LOGIC
+// function removes dashes and period from entered number
+var removeDashAndPeriod = function(userInput) {
+  var revisedNumber = [];
+  userInput.forEach(function(digit) {
+    if(digit === "-" || digit === ".") {
+      return;
+    } else {
+      revisedNumber.push(digit);
+    }
+  });
+  return revisedNumber;
+}
+// function checks digit of each if 0, 1 or divisible by 3
+var checkDigitType = function(number) {
+  var newMessageArray = [];
+  for(var i = 0; i <= number; i++) {
+    var numberToCheck = i.toString();
+    if(numberToCheck.includes("0")) {
+      newMessageArray.push("Boop!");
+    } else if(numberToCheck.includes("1")) {
+      newMessageArray.push("Beep!");
+    } else if(parseInt(numberToCheck) % 3 === 0) {
+      newMessageArray.push("I'm sorry Bill, I'm afraid I can't do that.");
+    } else {
+      newMessageArray.push(numberToCheck);
+    }
+  }
+  return newMessageArray;
+}
 
-
-// user interface logic
+// USER INTERFACE LOGIC
 $(document).ready(function() {
   $(".form").submit(function(event) {
     event.preventDefault();
+
     $(".no-white-space-message").hide();
     $(".message").empty();
+    
     var userInput = $(".userInput").val();
-
     if(userInput === "" || userInput === /\s/g) {
       $(".no-white-space-message").show();
     } else {
       var splitInput = userInput.split("");
-      var revisedNumber = [];
-      splitInput.forEach(function(digit) {
-        if(digit === "-" || digit === ".") {
-          return;
-        } else {
-          revisedNumber.push(digit);
-        }
-      });
+      var revisedNumber = removeDashAndPeriod(splitInput);
       var joinedRevisedNumber = parseInt(revisedNumber.join(""));
-      var range = [];
-      for(var i = 0; i <= joinedRevisedNumber; i++) {
-        var numberToCheck = i.toString();
-        if(numberToCheck.includes("0")) {
-          range.push("Boop!");
-        } else if(numberToCheck.includes("1")) {
-          range.push("Beep!");
-        } else if(parseInt(numberToCheck) % 3 === 0) {
-          range.push("I'm sorry Bill, I'm afraid I can't do that.");
-        } else {
-          range.push(numberToCheck);
-        }
-      }
-      var newMessage = range.join(" ");
+      var newMessageArray = checkDigitType(joinedRevisedNumber);
+      var newMessage = newMessageArray.join(" ");
       $(".message").text(newMessage);
     }
   });
