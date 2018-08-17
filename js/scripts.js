c449a12a31409ce25a76a04255eebf4a25d5c081
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-08-17 09:51:03 -0700

    add scritping to remove any - or . from input numbers

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,25 @@ $(document).ready(function() {
   $(".form").submit(function(event) {
     event.preventDefault();
     $(".white-space-message").hide();
+    $(".no-special-character-message").hide();
+    $(".message").text("");
     var userInput = $(".userInput").val();
+
     if(userInput === "" || userInput === /\s/g) {
-      $(".white-space-message").show();
+      $(".no-white-space-message").show();
     } else {
-      $(".message").text(userInput);
-      console.log(userInput);
+      var splitInput = userInput.split("");
+      var revisedNumber = [];
+      splitInput.forEach(function(digit) {
+        if(digit === "-" || digit === ".") {
+          return;
+        } else {
+          revisedNumber.push(digit);
+        }
+      });
+
+      $(".message").text(revisedNumber);
+      console.log(revisedNumber);
     }
   });
 });
